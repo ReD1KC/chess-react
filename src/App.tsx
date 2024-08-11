4133591fedf620555bbfdf435e32aab7f394cd0c
ReD1KC <tchirkok@gmail.com>
2024-08-11 13:16:18 +0700

    First steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import BoardComponent from "./components/BoardComponent";
+import { Board } from "./components/models/Board";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [board, setBoard] = useState(new Board());
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        restart();
+    });
+
+    function restart() {
+        const newBoard = new Board();
+        newBoard.initCells();
+        setBoard(newBoard);
+    }
+
+    return (
+        <>
+            <BoardComponent />
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,5 @@
+const CellComponent = () => {
+    return <div className="cell"></div>;
+};
+
+export default CellComponent;
